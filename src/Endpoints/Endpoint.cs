9a05ce5b688ac3e4a47fc0e66fdfa2d4f7487841
Sarah Guimaraes <saramg12s@gmail.com>
2025-10-26 19:40:34 -0300

    Pending: Finalizar endpoints

---
+++ src/Endpoints/Endpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace BugStore.Endpoints
+{
+    public static class Endpoint
+    {
+        public static void MapEndpoints(this WebApplication app)
+        {
+            var endpoints = app.MapGroup("");
+
+            endpoints.MapGroup("/")
+            .WithTags("Health Check")
+            .MapGet("/", () => new { message = "OK" });
+
+            endpoints.MapGroup("/customers")
+                .WithTags("Customers");
+        }
+
+        private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app)
+        where TEndpoint : IEndpoint
+        {
+            TEndpoint.Map(app);
+            return app;
+        }
+    }
+    
+    }
---
+++ src/Responses/Response.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BugStore.Responses
+{
+    public class Response<T>
+    {
+
+        public T? Data { get; private set; }
+        public string? Message { get; private set; }
+        
+        public Response(T? data, string message)
+        {
+            Data = data;
+            Message = message;
+        }
+        public Response(string message)
+        {
+            Message = message;
+        }
+        public Response(T? data)
+        {
+            Data = data;
+        }
+    }
+}
